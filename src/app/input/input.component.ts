790c14578f05dd35eea39dfa577fa58a12e70e80
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-02-21 08:52:51 -0500

    commit one. input component

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
