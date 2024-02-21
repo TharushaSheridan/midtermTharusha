ef68be4e32f175bdd09e83e62a539c185ba536ef
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-02-21 09:10:56 -0500

    commit tthree. footer , date

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -6,3 +6,21 @@ import { Router } from '@angular/router';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
 })
+export class InputComponent {
+  processorModel: string = '';
+  processorManufacturer: string = 'Intel';
+  windows11Compatibility: boolean = false;
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    const processorData = {
+      model: this.processorModel,
+      manufacturer: this.processorManufacturer,
+      windows11Compatibility: this.windows11Compatibility,
+    };
+
+    // Navigate to the output page and pass processorData as a parameter
+    this.router.navigate(['/output', processorData]);
+  }
+}
